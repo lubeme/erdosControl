153cdbd5ad0c3f0ed3e0213964a998440f3fb5b3
lubeme <luis@enigma.(none)>
2014-03-20 15:14:55 +0100

    fixed imports

--- netGrowControlTest.py
+++ netGrowControlTest.py
@@ -7,6 +7,6 @@ controlersPerEdgesPerIters={}
 for k in xrange (0, iters):
     if k>0 and k%(iters/10)==0:
         print "iteracion:",k
-    controlersPerEdgesPerIters[k]= netGen.net_grow_control(n)
+    controlersPerEdgesPerIters[k]= netGrowControl.net_grow_control(n)
 with open('data.json', 'wb') as fp:
     json.dump(controlersPerEdgesPerIters, fp)
